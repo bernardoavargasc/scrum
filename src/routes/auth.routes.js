014d94773baa9154040ca7fd2f5f81eec24542ca
Franks0200 <franciscojavierg213@gmail.com>
2026-07-06 17:34:45 -0600

    Add files via upload

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const c = require('../controllers/auth.controller');
 router.post('/login',      c.login);
+router.post('/google',     c.google);
 router.post('/recuperar',  c.recuperar);
 router.put('/cambiar-pass', c.cambiarPass);
 module.exports = router;
--- src/routes/mensaje.routes.js
+++ src/routes/mensaje.routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const c = require('../controllers/mensaje.controller');
-router.get('/',  c.listar);
-router.post('/', c.crear);
+router.get('/',           c.listar);
+router.get('/no-leidos',  c.noLeidos);
+router.post('/',          c.crear);
+router.put('/leidos',     c.marcarLeidos);
 module.exports = router;
